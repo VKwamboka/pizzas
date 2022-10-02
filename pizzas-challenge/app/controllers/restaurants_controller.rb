414ac733193308d843bab54a94658fef54b56b50
verah <verahnyagaka101@gmail.com>
2022-10-02 21:23:48 +0300

    added get show for restaurants

--- pizzas-challenge/app/controllers/restaurants_controller.rb
+++ pizzas-challenge/app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+     # GET /restaurant
+  def index
+    restaurants = Restaurant.all
+    render json: restaurants
+  end
+
+  # GET /restaurant/:id
+  def show
+    restaurant = Restaurant.find_by(id: params[:id])
+    if restaurant
+      render json: restaurant, include: :pizzas
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
 end
--- pizzas-challenge/app/models/restaurant_pizza.rb
+++ pizzas-challenge/app/models/restaurant_pizza.rb
@@ -1,6 +1,9 @@
 class RestaurantPizza < ApplicationRecord
+    
+    # must have a `price` between 1 and 30
+    validates :price, numericality: { in: 0..30}
+
     belongs_to :restaurant
     belongs_to :pizza
-
-
+    
 end
